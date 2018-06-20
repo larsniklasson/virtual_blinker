717ff3d7aae0bb76fb10eabf029de661687e6826
Ibrahim Fayaz <fayaz@student.chalmers.se>
2018-06-20 21:03:40 +0200

    removed acceleration

--- simulation/sim.py
+++ simulation/sim.py
@@ -261,16 +261,16 @@ class Car:
         
         # follow speed profile
         targetspeed = self.course.getSpeed(self.x, self.y, self.theta, self.Is)
-        if self.id == 0 and not self.man_init:
-            targetspeed = 999999
+        # if self.id == 0 and not self.man_init:
+        #     targetspeed = 999999
         if targetspeed < self.speed:
             targetacc = self.course.catchup_deacc
             self.speed += dt*targetacc/SLOWDOWN
             self.speed = max(self.speed, targetspeed)
         else:
             targetacc = self.course.catchup_acc
-            if self.id == 0 and not self.man_init:
-                targetacc = 4
+            # if self.id == 0 and not self.man_init:
+            #     targetacc = 4
             self.speed += dt*targetacc/SLOWDOWN
             self.speed = min(self.speed, targetspeed)
         
