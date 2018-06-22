f2bc518c6c81663fc9ee7a0c1ad58e09a29b8c6c
Ibrahim Fayaz <fayaz@student.chalmers.se>
2018-06-22 17:01:36 +0200

    preparing for scenario A

--- simulation/sim.py
+++ simulation/sim.py
@@ -261,7 +261,7 @@ class Car:
         
         # follow speed profile
         targetspeed = self.course.getSpeed(self.x, self.y, self.theta, self.Is)
-        if self.id == 0 and not self.granted:
+        if self.id == 0 and self.granted:
             targetspeed = 999999
         if targetspeed < self.speed:
             targetacc = self.course.catchup_deacc
@@ -269,8 +269,8 @@ class Car:
             self.speed = max(self.speed, targetspeed)
         else:
             targetacc = self.course.catchup_acc
-            if self.id == 0 and not self.granted:
-                targetacc = 4
+            if self.id == 0 and self.granted:
+                targetacc = 1
             self.speed += dt*targetacc/SLOWDOWN
             self.speed = min(self.speed, targetspeed)
         
